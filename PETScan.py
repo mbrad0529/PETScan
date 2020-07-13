e54cdc639232ea93cea4e7047fe632af8a85d5d7
Matt <matthew.bradley0529@gmail.com>
2020-07-12 23:51:23 -0400

    Added aliases of -r and --report for Output option

--- PETScan.py
+++ PETScan.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Analyzes target PE file and reports various file attributes.")
 parser.add_argument("file", help="File to analyze")
 parser.add_argument("-v", "--verbose", help="Increase command line output verbosity.", action="store_true")
-parser.add_argument("-o", "--output", help="Output file to write results to, default is report.txt", nargs="?",
-                    dest="outfile", action="store", default="report.txt", const="report.txt")
+parser.add_argument("-o", "--output", "-r", " --report", help="Output file to write results to, default is report.txt",
+                    nargs="?", dest="outfile", action="store", default="report.txt", const="report.txt")
 args = parser.parse_args()
 
