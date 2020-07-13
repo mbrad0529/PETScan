3903a78d7e43e78636705194967862495f554582
Matt <matthew.bradley0529@gmail.com>
2020-07-12 23:50:27 -0400

    Wrote ArgumentParser for Verbose and Output command line options

---
+++ PETScan.py
@@ -0,0 +1,10 @@
+import pefile
+import argparse
+
+parser = argparse.ArgumentParser(description="Analyzes target PE file and reports various file attributes.")
+parser.add_argument("file", help="File to analyze")
+parser.add_argument("-v", "--verbose", help="Increase command line output verbosity.", action="store_true")
+parser.add_argument("-o", "--output", help="Output file to write results to, default is report.txt", nargs="?",
+                    dest="outfile", action="store", default="report.txt", const="report.txt")
+args = parser.parse_args()
+
